cecf739a6570a91ff27c1fb64782a40f95af9326
abbassaid <abbas-s-7@hotmail.com>
2023-09-30 10:57:02 -0400

    changed the name of the directory

--- 101239441_COMP3123-exec04/101239441_COMP3123-exec04.js
+++ 101239441_COMP3123-exec04/101239441_COMP3123-exec04.js
@@ -7,15 +7,23 @@ app.get("/hello", (req,res) => {
 })
 
 app.get("/user", (req,res) => {
-
+    // Seeing if the first name and last name was sent as a query
     const { firstname, lastname } = req.query
 
+    // Storing it in a user object
     const user = {
         firstname,
         lastname
     }
 
-    res.json(user)
+    // If there is no firstname and last name defined we will send an error
+    // message for them
+    if (user.firstname === undefined || user.lastname === undefined) {
+        res.send("Required firstname and lastname as query parameter")
+    }
+    else {
+        res.json(user)
+    }
 })
 
 
@@ -31,4 +39,4 @@ app.post("/user/:firstname/:lastname", (req, res) => {
     res.json(user)
 })
 
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
+app.listen(port, () => console.log(`Express app running on http://localhost:${port}!`));
